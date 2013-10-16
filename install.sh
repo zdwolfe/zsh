bcf5c4c0adf66899557604f4665c5eea50e701d3
zdwolfe <wolfe.zach@gmail.com>
2013-10-16 13:26:15 -0400

    Rewrite install plugin and make autoupdate config changes

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATE=`date +%s`
+
 git submodule init
 git submodule update
-mkdir ~/.zsh
-rm -rf ~/.zsh/oh-my-zsh
-ln -sf $DIR/oh-my-zsh ~/.zsh/oh-my-zsh
-rm -f ~/.zshrc-backup
+
+if [ -d ~/.zsh ] 
+then
+  mv ~/.zsh ~/.zsh.$DATE
+fi
+ln -sf $DIR ~/.zsh
+
+if [ -f ~/.zshrc ]
+then
+  mv ~/.zshrc ~/.zshrc.$DATE
+fi
 ln -sf $DIR/zshrc ~/.zshrc
+
 echo "Enter password for $USER to change default shell"
 chsh -s $(which zsh)
 echo "Done. May have to reboot before default shell is applied"
